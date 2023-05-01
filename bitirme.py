89f6184efb76082b6a2ec8a62d0cae3d0b92d839
KaanIpek <72802777+KaanIpek@users.noreply.github.com>
2023-05-01 15:49:57 +0300

    v1

--- bitirme.py
+++ bitirme.py
@@ -50,7 +50,12 @@ def train_SVC_model(data, labels):
 
 
     X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
-   
+    X_train = vectorizer.fit_transform(X_train)
+    X_test = vectorizer.transform(X_test)
+    tfidf = TfidfTransformer()
+    X_train = tfidf.fit_transform(X_train)
+    X_test = tfidf.transform(X_test)
+
 
     model.fit(X_train, y_train)
     accuracy = model.score(X_test, y_test)
